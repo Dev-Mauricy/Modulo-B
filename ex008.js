31969ff49280137b090810bf531db7cd39601a2f
Dev-Mauricy <mauricyleo@gmail.com>
2022-10-05 20:52:48 -0300

    Console log e primeros programas

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do carro é ${vel}Km/h`)
+if(vel>60){
+    console.log('Você passou do limite de velocidade, multado!')
+}
+console.log('Use sempre o cinto de segurança')
