cf2946d0a59f5bb6c42556959f701cb21e18949c
Dev-Mauricy <mauricyleo@gmail.com>
2022-10-07 17:38:38 -0300

    Projeto part 2

---
+++ Projeto01JS/script.js
@@ -0,0 +1,23 @@
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = 12 //data.getHours()
+//var minutos = data.getMinutes()
+msg.innerHTML = `Agora são ${hora}h`
+if (hora >= 0 && hora <12){
+    //Bom dia
+    img.src = 'imagens/manha.jpg'
+    document.body.style.background= '#dfae76'
+}
+else if (hora >= 12 && hora <18) {
+    //Boa tarde
+    img.src = 'imagens/tarde.jpg'
+    document.body.style.background= '#738d9c'
+}
+else{
+    //Boa noite
+    img.src = 'imagens/noite.jpg'
+    document.body.style.background = ' #342a43'
+}
+}
