7a874420da8f06231e6ed4241477735d59a96f66
Dev-Mauricy <mauricyleo@gmail.com>
2022-10-08 14:33:43 -0300

    Projeto02JS

--- Projeto01JS/script.js
+++ Projeto01JS/script.js
@@ -2,7 +2,7 @@ function carregar(){
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
-var hora = 12 //data.getHours()
+var hora = 15 //data.getHours()
 //var minutos = data.getMinutes()
 msg.innerHTML = `Agora são ${hora}h`
 if (hora >= 0 && hora <12){
